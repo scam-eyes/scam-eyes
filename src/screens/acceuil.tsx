2e7beb0b1558fcc4d8fb2aa942490ef946251c23
JosephfranckG <jgoumdebengassia@yahoo.com>
2022-05-01 19:56:48 +0100

    creation des sous repertoires

---
+++ src/screens/acceuil.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./acceuil.css";
+
+class Accueil extends React.Component{
+    render(){
+
+        return (
+            <div className="global">
+                bonjour
+            </div>
+        )
+    }
+}
+
+export default Accueil;
